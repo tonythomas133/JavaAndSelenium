5fa920498827fb265fa7c49f9bee7ba01aa918bd
Tony <thomasmtony@gmail.com>
2022-06-20 11:17:30 +0100

    20/06/2022

--- SeleniumTrainingJava/src/automation/TC10_VerifyIframes.java
+++ SeleniumTrainingJava/src/automation/TC10_VerifyIframes.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TC9_VerifyIframes {
+public class TC10_VerifyIframes {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "F:\\selenium training\\drivers\\chromedriver.exe");
